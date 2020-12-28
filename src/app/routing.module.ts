503afb0c1eb1d1daad87bf419d111e1f00648dad
lilit.mazmanyan <lilit.mazmanyan@synisys.com>
2020-12-28 14:20:02 +0400

    Code cleanup, imports optimization Added Shopping List module

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,9 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {AuthComponent} from './auth/auth.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'shopping-list', component: ShoppingListComponent},
   {path: 'auth', component: AuthComponent}
 ];
 
@@ -14,5 +11,5 @@ const appRoutes: Routes = [
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
+export class RoutingModule {
 }
